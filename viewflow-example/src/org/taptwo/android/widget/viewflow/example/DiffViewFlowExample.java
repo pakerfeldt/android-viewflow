58cb67a1193169ea5744203992efb799956e8799
Afzal Najam <afzal411@gmail.com>
2011-07-22 01:28:57 -0400

    DiffView with a populated ListView inside
    Signed-off-by: Afzal Najam <afzal411@gmail.com>

--- viewflow-example/src/org/taptwo/android/widget/viewflow/example/DiffViewFlowExample.java
+++ viewflow-example/src/org/taptwo/android/widget/viewflow/example/DiffViewFlowExample.java
@@ -5,28 +5,38 @@ import org.taptwo.android.widget.ViewFlow;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class DiffViewFlowExample extends Activity {
 
 	private ViewFlow viewFlow;
+	private ListView listView;
 
 	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setTitle(R.string.title_title);;
-        
+        setTitle(R.string.diff_title);
         setContentView(R.layout.title_layout);
         
 		viewFlow = (ViewFlow) findViewById(R.id.viewflow);
-
         DiffAdapter adapter = new DiffAdapter(this);
-
         viewFlow.setAdapter(adapter);
 		TitleFlowIndicator indicator = (TitleFlowIndicator) findViewById(R.id.viewflowindic);
 		indicator.setTitleProvider(adapter);
 		viewFlow.setFlowIndicator(indicator);
+		
+		/** To populate ListView in diff_view1.xml */
+		listView = (ListView) findViewById(R.id.listView1);
+		String[] names = new String[] { "Cupcake", "Donut", "Eclair", "Froyo",
+				"Gingerbread", "Honeycomb", "IceCream Sandwich"};
+		// Create an ArrayAdapter, that will actually make the Strings above
+		// appear in the ListView
+		listView.setAdapter(new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, names));
+		
     }
-
- 
 }
