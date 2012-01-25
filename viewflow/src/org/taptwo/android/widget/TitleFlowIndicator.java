c921f4764208a86f2596472f036933f20cc2ea35
Patrik Åkerfeldt <patrik.akerfeldt@gmail.com>
2012-01-25 13:25:22 +0100

    Closes #58

--- viewflow/src/org/taptwo/android/widget/TitleFlowIndicator.java
+++ viewflow/src/org/taptwo/android/widget/TitleFlowIndicator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2011 Patrik �kerfeldt
+ * Copyright (C) 2011 Patrik Åkerfeldt
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -361,6 +361,7 @@ public class TitleFlowIndicator extends TextView implements FlowIndicator {
 	@Override
 	public void setViewFlow(ViewFlow view) {
 		viewFlow = view;
+		currentPosition = view.getSelectedItemPosition();
 		invalidate();
 	}
 
